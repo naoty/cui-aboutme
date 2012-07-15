becd88a94d9fa242f5f2307306d0bb8c528e6f3f
naoty <naoty.k@gmail.com>
2012-07-15 16:19:40 +0900

    add rails project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+CuiAboutme::Application.config.secret_token = 'f73faac7b0baae1dafe354358bde3d99fa785bbee456fc691d488645fc845a9c26ec905bb86cb55c8221f3fdfec2ba91440c9e12d68e7d05a0627e3ed222f7ba'
