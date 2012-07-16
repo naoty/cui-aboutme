a7cb1d4f4cc0b988aa2b2bbbc7cdf425c76181c0
naoty <naoty.k@gmail.com>
2012-07-16 15:51:29 +0900

    add users#update

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -15,4 +15,15 @@ class UsersController < ApplicationController
       render 'error', status: :unprocessable_entity
     end
   end
+
+  def update
+    @user = User.find(params[:id])
+    if not @user.authenticate(params[:password])
+      render 'unauthorized', status: :unauthorized
+    elsif @user.update_attributes(params)
+      render 'success', status: :ok
+    else
+      render 'error', status: :unprocessable_entity
+    end
+  end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -4,20 +4,30 @@ class User
 
   field :name
   field :encrypted_password
+  field :salt
   field :_id, type: String, default: -> { name }
 
-  attr_accessor :password, :salt
+  attr_accessor :password
 
   index({ name: 1 }, { unique: true })
   validates :name, presence: true, uniqueness: true
 
   before_save :encrypt_password
 
-  INVISIBLE_ITEMS = ['_id', 'created_at', 'updated_at', 'encrypted_password']
+  INVISIBLE_ITEMS = ['_id', 'created_at', 'updated_at', 'encrypted_password', 'salt']
 
   def self.new(params)
     user_params = params.reject {|field| ['controller', 'action'].include?(field) }
     super(user_params)
+  end
+
+  def update_attributes(params)
+    user_params = params.reject {|field| ['controller', 'action'].include?(field) }
+    super(user_params)
+  end
+
+  def authenticate(password)
+    self.encrypted_password == BCrypt::Engine.hash_secret(password, self.salt)
   end
 
   def items
@@ -28,8 +38,8 @@ class User
 
   def encrypt_password
     if @password.present?
-      @salt = BCrypt::Engine.generate_salt
-      self.encrypted_password = BCrypt::Engine.hash_secret(@password, @salt)
+      self.salt = BCrypt::Engine.generate_salt
+      self.encrypted_password = BCrypt::Engine.hash_secret(@password, self.salt)
     end
   end
 end
