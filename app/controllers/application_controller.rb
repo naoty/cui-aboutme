b7ab4137978ebb45153bf63f253150342e9895fc
naoty <naoty.k@gmail.com>
2012-07-16 15:25:09 +0900

    add 404.text.erb

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,11 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
+
+  rescue_from Mongoid::Errors::DocumentNotFound, with: :error_404
+
+  private
+
+  def error_404
+    render '404'
+  end
 end
