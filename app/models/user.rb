0352486d5996cfd21fbd1b7957188decd046181f
naoty <naoty.k@gmail.com>
2012-07-16 13:39:39 +0900

    save a encrypted password

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,8 +3,22 @@ class User
   include Mongoid::Timestamps
 
   field :name
+  field :encrypted_password
   field :_id, type: String, default: -> { name }
+
+  attr_accessor :password, :salt
 
   index({ name: 1 }, { unique: true })
   validates :name, presence: true, uniqueness: true
+
+  before_save :encrypt_password
+
+  private
+
+  def encrypt_password
+    if @password.present?
+      @salt = BCrypt::Engine.generate_salt
+      self.encrypted_password = BCrypt::Engine.hash_secret(@password, @salt)
+    end
+  end
 end
