8d6848f96773489841385e3cef73270f128a7597
naoty <naoty.k@gmail.com>
2012-07-27 21:02:21 +0900

    print description at users#man

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -6,4 +6,13 @@ module ApplicationHelper
     text.prepend(INDENT * n)
   end
 
+  def wrap(text, width = 70, n = 1)
+    lines = text.scan(/.{1,#{width}}/)
+    lines.map! do |line|
+      line.prepend(INDENT * n)
+    end
+    wrapped_text = lines.join("\n")
+    return lines.present? ? wrapped_text : indent(text)
+  end
+
 end
